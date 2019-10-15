2db1986820874729a7f05aef7750778423a3c5a0
Gareth Ellis <gareth.ellis@iconplc.com>
2019-10-15 17:07:35 +0100

    #4 Adds days() function

--- src/functions.php
+++ src/functions.php
@@ -12,6 +12,11 @@ function minutes(int $minutes): Minutes
     return new Minutes($minutes);
 }
 
+function days(int $days): Days
+{
+    return new Days($days);
+}
+
 function seconds(int $seconds): Seconds
 {
     return new Seconds($seconds);
